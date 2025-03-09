ca3498bf806f8ffc5013e5af125f93739b77c6f7
melodycurran <mcurran@byupathway.edu>
2025-03-09 17:41:28 -0600

    Cleaned up codes

--- week1-learning-activity/scripts/sample.js
+++ week1-learning-activity/scripts/sample.js
@@ -1,7 +1,6 @@
 const app = document.getElementById('app');
 
 function Header({ title }) {
-    console.log(title)
     return <h1>{title ? title : 'Default title'}</h1>;
 }
 
@@ -10,20 +9,22 @@ function Homepage() {
     const [likes, setLikes] = React.useState(0);
 
     function handleClick() {
+
         setLikes(likes + 1);
     }
 
-    return (<div>
-        <Header title="Develop. Preview. Ship." />
-        <Header title="" />
-
-        <ul>
-            {names.map((name) => (
-                <li key={name}>{name}</li>
-            ))}
-        </ul>
-        <button onClick={handleClick}>Like ({likes})</button>
-    </div>);
+    return (
+        <div>
+            <Header title="Develop. Preview. Ship." />
+            <Header title="Another title" />
+            <ul>
+                {names.map((name) => (
+                    <li key={name}>{name}</li>
+                ))}
+            </ul>
+            <button onClick={handleClick}>Like ({likes})</button>
+        </div>
+    );
 }
 
 const root = ReactDOM.createRoot(app);
